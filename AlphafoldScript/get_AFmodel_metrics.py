730b3972ca724edd76e4c1ffc99ccf44f112650b
Levurmion <122384242+Levurmion@users.noreply.github.com>
2023-05-13 23:30:46 +0100

    pickled

--- AlphafoldScript/get_AFmodel_metrics.py
+++ AlphafoldScript/get_AFmodel_metrics.py
@@ -4,6 +4,11 @@ import sys
 import pandas as pd
 import argparse
 import numpy as np
+from betterpicklejar import *
+
+PickleShelf('../pickle_shelf')
+
+jar = PickleJar()
 
 # get command line arguments
 parser = argparse.ArgumentParser(
@@ -20,6 +25,7 @@ parser.add_argument('-r', '--radius', help='A comma-separated argument string of
 parser.add_argument('--pae_query_only', help='Flag to calculate the average PAE using residue pairs involving the query only. Default=False.')
 parser.add_argument('--plddt_window', help='A comma-separated argument string of the window size (must be odd numbers) to calculate plDDT scores. ("3,5,7")')
 parser.add_argument('--scrap_ori', help='Flag on whether to keep the original columns. Default= False.')
+parser.add_argument('--pickle', help='Use pickle to save alphafold models for future runs.')
 
 args = parser.parse_args()
 
@@ -31,6 +37,7 @@ RADIUS: list = [5] if args.radius == None else [float(radius) for radius in args
 PAE_QUERY_ONLY: bool = True if args.pae_query_only != None else False
 PLDDT_WINDOW = [float(radius) for radius in args.plddt_window.split(',')] if isinstance(args.plddt_window, str) else False
 SCRAP_COL: bool = True if args.scrap_ori != None else False
+pickle_name = False if args.pickle == None else args.pickle
 
 
 # input check
@@ -44,7 +51,7 @@ if PLDDT_WINDOW != False:
          pass
 
 # script start
-AF_MODELS: dict = {}
+
 
 VARIANTS_DF: pd.DataFrame = pd.read_csv(INPUT_PATH, sep='\t')
 
@@ -56,25 +63,39 @@ PROBLEM_MODELS = []
 PAE_NO_MATCH = []
 
 # load all required Alphafold models
-for uniprot in UNIPROT_IDS:
+
+def load_AF_models():
    
-   MODEL_PDB = f'AF-{uniprot}-F1-model_v4.pdb'
-   MODEL_PAE = f'AF-{uniprot}-F1-predicted_aligned_error_v4.json'
-   MODEL_PDB_PATH = os.path.join(PDB_PATH, MODEL_PDB)
-   MODEL_PAE_PATH = os.path.join(PAE_PATH, MODEL_PAE)
+   AF_MODELS: dict = {}
    
-   print(MODEL_PDB_PATH)
-   print(MODEL_PAE_PATH)
+   for uniprot in UNIPROT_IDS:
+      
+      MODEL_PDB = f'AF-{uniprot}-F1-model_v4.pdb'
+      MODEL_PAE = f'AF-{uniprot}-F1-predicted_aligned_error_v4.json'
+      MODEL_PDB_PATH = os.path.join(PDB_PATH, MODEL_PDB)
+      MODEL_PAE_PATH = os.path.join(PAE_PATH, MODEL_PAE)
+      
+      print(MODEL_PDB_PATH)
+      print(MODEL_PAE_PATH)
+      
+      try:
+         AF_MODELS[uniprot] = AlphafoldModel(MODEL_PDB_PATH, MODEL_PAE_PATH)
+         print(f'Loading {uniprot} into memory... \r', end='')
+      except FileNotFoundError:
+         print(f'{uniprot} not found...')
+         PROBLEM_MODELS.append(uniprot)
+      except ValueError:
+         print(f'{MODEL_PAE_PATH} not found...')
+         PAE_NO_MATCH.append(uniprot)
    
-   try:
-      AF_MODELS[uniprot] = AlphafoldModel(MODEL_PDB_PATH, MODEL_PAE_PATH)
-      print(f'Loading {uniprot} into memory... \r', end='')
-   except FileNotFoundError:
-      print(f'{uniprot} not found...')
-      PROBLEM_MODELS.append(uniprot)
-   except ValueError:
-      print(f'{MODEL_PAE_PATH} not found...')
-      PAE_NO_MATCH.append(uniprot)
+   return AF_MODELS
+
+AF_MODELS = None
+
+if pickle_name == False:
+   AF_MODELS = load_AF_models()
+else:
+   AF_MODELS = jar.pickle(load_AF_models, pickle_name=pickle_name)
 
 # initialize output columns as dict
 OUTPUT_COLS = {}
@@ -133,31 +154,35 @@ for variant in VARIANTS_DF.itertuples():
    
    WTaa = getattr(variant, 'WT')
    
-   if modelNotFound == True:
-      PROBLEM_VARIANTS.append(int(getattr(variant, 'Index')))
-   elif WTaa != MODEL.get_residue(queryPos)[0] or queryPos == None:
-      PROBLEM_VARIANTS.append(int(getattr(variant, 'Index')))
-   else:
-      for rad in RADIUS:
-         if modelNotFound == False:
-            PAEcolname = gen_pae_colname(rad)
-            PAE_in_rad = np.round(MODEL.get_local_PAE(queryPos, rad, with_query_only=PAE_QUERY_ONLY),3)
-            OUTPUT_COLS[PAEcolname].append(PAE_in_rad)
+   try:
+      if modelNotFound == True:
+         PROBLEM_VARIANTS.append(int(getattr(variant, 'Index')))
+      elif WTaa != MODEL.get_residue(queryPos)[0] or queryPos == None:
+         PROBLEM_VARIANTS.append(int(getattr(variant, 'Index')))
+      else:
+         for rad in RADIUS:
+            if modelNotFound == False:
+               PAEcolname = gen_pae_colname(rad)
+               PAE_in_rad = np.round(MODEL.get_local_PAE(queryPos, rad, with_query_only=PAE_QUERY_ONLY),3)
+               OUTPUT_COLS[PAEcolname].append(PAE_in_rad)
 
-            PLDDTcolname = gen_plddt_colname(rad)
-            PLDDT_in_rad = np.round(MODEL.get_local_plddt(queryPos, rad),3)
-            OUTPUT_COLS[PLDDTcolname].append(PLDDT_in_rad)
-         else:
-            pass
-      
-      if isinstance(PLDDT_WINDOW, list):
-         if modelNotFound == False:
-            for win in PLDDT_WINDOW:
-               PLDDTcolname = gen_plddt_win_colname(win)
-               PLDDT_for_win = np.round(MODEL.get_plddt_window(queryPos, win)[0], 3)
-               OUTPUT_COLS[PLDDTcolname].append(PLDDT_for_win)
-         else:
-            pass
+               PLDDTcolname = gen_plddt_colname(rad)
+               PLDDT_in_rad = np.round(MODEL.get_local_plddt(queryPos, rad),3)
+               OUTPUT_COLS[PLDDTcolname].append(PLDDT_in_rad)
+            else:
+               pass
+         
+         if isinstance(PLDDT_WINDOW, list):
+            if modelNotFound == False:
+               for win in PLDDT_WINDOW:
+                  PLDDTcolname = gen_plddt_win_colname(win)
+                  PLDDT_for_win = np.round(MODEL.get_plddt_window(queryPos, win)[0], 3)
+                  OUTPUT_COLS[PLDDTcolname].append(PLDDT_for_win)
+            else:
+               pass
+   
+   except ValueError:
+      PROBLEM_VARIANTS.append(int(getattr(variant, 'Index')))
 
 VARIANTS_DF_GOOD = VARIANTS_DF.drop(PROBLEM_VARIANTS)
 VARIANTS_DF_GOOD = VARIANTS_DF_GOOD.assign(**OUTPUT_COLS)
